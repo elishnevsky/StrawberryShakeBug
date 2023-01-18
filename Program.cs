fe6d2e606275bcf38bbe382a5d0a80da236f17dc
Eugene Lishnevsky <elishnevsky@gmail.com>
2023-01-17 22:08:07 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using StrawberryShakeBug;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services
+    .AddCryptoClient()
+    .ConfigureHttpClient(c => c.BaseAddress = new Uri("https://api-crypto-workshop.chillicream.com/graphql"));
+
+await builder.Build().RunAsync();
